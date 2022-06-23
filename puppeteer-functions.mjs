c2c8116c6764d0456194645fc88dc83386ecf1f3
robin@mordasiewicz.com <robin@mordasiewicz.com>
2022-06-23 10:07:05 -0400

    updating intro

--- puppeteer-functions.mjs
+++ puppeteer-functions.mjs
@@ -141,5 +141,24 @@
       throw new Error('Timed out');
     }
 
-module.exports = { waitForSelectors, scrollIntoViewIfNeeded, waitForConnected, waitForInViewport, waitForSelector, waitForElement, querySelectorsAll, querySelectorAll, waitForFunction };
+async function autoScroll(page){
+    await page.evaluate(async () => {
+        await new Promise((resolve, reject) => {
+            var totalHeight = 0;
+            var distance = 100;
+            var timer = setInterval(() => {
+                var scrollHeight = document.body.scrollHeight;
+                window.scrollBy(0, distance);
+                totalHeight += distance;
+
+                if(totalHeight >= scrollHeight){
+                    clearInterval(timer);
+                    resolve();
+                }
+            }, 100);
+        });
+    });
+}
+
+module.exports = { autoScroll,waitForSelectors, scrollIntoViewIfNeeded, waitForConnected, waitForInViewport, waitForSelector, waitForElement, querySelectorsAll, querySelectorAll, waitForFunction };
 
